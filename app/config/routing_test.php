6edbace9bd1a7d6ba6ab38fab5246f4b3e031a22
Wong Hoi Sing Edison <hswong3i@gmail.com>
2014-11-21 21:45:44 +0800

    Fix bind for dummy route.

--- app/config/routing_test.php
+++ app/config/routing_test.php
@@ -12,32 +12,32 @@
 require __DIR__.'/routing_dev.php';
 
 $app->post('/dummy/v1.0/push/register', 'authbucket_push.push_controller:registerAction')
-    ->bind('api_push_register');
+    ->bind('dummy_push_register');
 
 $app->post('/dummy/v1.0/push/unregister', 'authbucket_push.push_controller:unregisterAction')
-    ->bind('api_push_unregister');
+    ->bind('dummy_push_unregister');
 
 $app->post('/dummy/v1.0/push/send', 'authbucket_push.push_controller:sendAction')
-    ->bind('api_push_send');
+    ->bind('dummy_push_send');
 
 foreach (array('service', 'device', 'message') as $type) {
     $app->post('/dummy/v1.0/'.$type.'.{_format}', 'authbucket_push.'.$type.'_controller:createAction')
-        ->bind('api_'.$type.'_create')
+        ->bind('dummy_'.$type.'_create')
         ->assert('_format', 'json|xml');
 
     $app->get('/dummy/v1.0/'.$type.'/{id}.{_format}', 'authbucket_push.'.$type.'_controller:readAction')
-        ->bind('api_'.$type.'_read')
+        ->bind('dummy_'.$type.'_read')
         ->assert('_format', 'json|xml');
 
     $app->put('/dummy/v1.0/'.$type.'/{id}.{_format}', 'authbucket_push.'.$type.'_controller:updateAction')
-        ->bind('api_'.$type.'_update')
+        ->bind('dummy_'.$type.'_update')
         ->assert('_format', 'json|xml');
 
     $app->delete('/dummy/v1.0/'.$type.'/{id}.{_format}', 'authbucket_push.'.$type.'_controller:deleteAction')
-        ->bind('api_'.$type.'_delete')
+        ->bind('dummy_'.$type.'_delete')
         ->assert('_format', 'json|xml');
 
     $app->get('/dummy/v1.0/'.$type.'.{_format}', 'authbucket_push.'.$type.'_controller:listAction')
-        ->bind('api_'.$type.'_list')
+        ->bind('dummy_'.$type.'_list')
         ->assert('_format', 'json|xml');
 }
